8b9feb35394bc2cf2f35f18879803a6c5dee0f8f
jericholawson <jboloslawson@gmail.com>
2025-08-06 13:15:41 -0500

    copy sds192 website

---
+++ sds192/f25/R/calendar.R
@@ -0,0 +1,22 @@
+print_calendar <- function(major_due_dates, annotation_due_dates, m) {
+
+  major_due_dates_m <- major_due_dates %>% filter(month == m) %>%
+    mutate(assignment = str_replace_all(assignment, "\\[|\\]", "")) %>%
+    mutate(assignment = str_replace_all(assignment, "\\s*\\([^\\)]+\\)", ""))
+  annotation_due_dates_m <- annotation_due_dates %>% filter(month == m)
+  
+  
+  print(calendR(year = 2025, 
+          month = m,       # Year and month
+          start = "M",     # Start the week on Sunday
+          text = major_due_dates_m$assignment, 
+          text.pos = major_due_dates_m$day,       # Days of the month where to put the texts 
+          text.size = 2,               # Font size of the text
+          text.col = 1,
+          special.days = annotation_due_dates_m$day,
+          special.col = "lightblue",
+          title.size = 10,
+          font.family = "sans",             # Font family of all the texts                 
+          weeknames = c("M", "T", "W", "T", "F", "S", "S")))
+
+}
